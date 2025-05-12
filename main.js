ff59b634a4eb08ae24b82f031d0ca3cc2667abed
DmytroHalushka <galushkadmytro@gmail.com>
2025-05-12 19:04:31 +0300

    aaaaaa

---
+++ main.js
@@ -0,0 +1,6 @@
+//src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"
+let kotiki = document.querySelector('.kotiki')
+let kotiki2 = document.querySelector('.kotiki2')
+/*window.onload = function() {
+    kotiki2.style.transform = 'translateY(-70px)'
+}*/
